cc5749f49561593e8ebf716d472a3641864250a7
Patrick Piker <patrick@klingaa.dk>
2023-11-29 07:52:37 +0100

    added CalculationHelper class with CalculateHoursToDouble() and GetRealTimeKilowattsInHours()

---
+++ TecEnergy.WebAPI/Helpers/CalculationHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace TecEnergy.WebAPI.Helpers;
+
+public class CalculationHelper
+{
+    public static double CalculateHoursToDouble(DateTime startTime, DateTime endTime)
+    {
+        TimeSpan duration = endTime - startTime;
+        var hours = duration.TotalHours;
+        return hours;
+    }
+
+    public static double GetRealTimeKilowattsInHours(int watts, DateTime startTime, DateTime endTime)
+    {
+        var hours = CalculateHoursToDouble(startTime, endTime);
+
+        var kWh = (watts * hours) / 1000;
+        return kWh;
+    }
+}
