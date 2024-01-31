1000bb6d864f92e80995acebf2abf519814b558b
ChimpChonk <sanjit.poudel28@gmail.com>
2024-01-31 13:04:53 +0100

    new ip test

--- TecEnergy.Database/Migrations/20240122072808_fuck.Designer.cs
+++ TecEnergy.Database/Migrations/20240122072808_fuck.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TecEnergy.Database;
 
@@ -11,9 +12,11 @@ using TecEnergy.Database;
 namespace TecEnergy.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240122072808_fuck")]
+    partial class fuck
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TecEnergy.Database/Migrations/20240122072808_fuck.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TecEnergy.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class fuck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeasurementType",
+                table: "EnergyMeters");
+
+            migrationBuilder.DropColumn(
+                name: "ReadingFrequency",
+                table: "EnergyMeters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MeasurementType",
+                table: "EnergyMeters",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReadingFrequency",
+                table: "EnergyMeters",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
