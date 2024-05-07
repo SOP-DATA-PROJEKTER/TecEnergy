1d1952e250027e443365579e2fd7bc0dfc69c63a
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-05-07 12:42:09 +0200

    Update: connection strings; Todo: Remove hardcoded strings and use settings file; Removed: unused imports;

--- EnergyApp/app/src/app/services/graph.service.ts
+++ EnergyApp/app/src/app/services/graph.service.ts
@@ -1,44 +1,35 @@
-import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { DateValue } from '../models/DateValue';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GraphService {
-
-
   constructor(private http: HttpClient) { }
 
-
-  url : string = "http://192.168.5.133:2050/api/Graph"
   // url : string = "https://localhost:7227/api/Graph"
+  // url: string = 'http://192.168.21.7:2050/api/Graph';
+  url: string = 'http://10.233.134.113/api/Graph/';
 
-
-  
-  getDailyGraphData(roomId: string, date: Date): Observable<DateValue[]>
-  {
-    return this.http.get<DateValue[]>(`${this.url}/Daily/${roomId}/${this.dateConverter(date)}`);
+  getDailyGraphData(roomId: string, date: Date): Observable<DateValue[]> {
+    return this.http.get<DateValue[]>(
+      `${this.url}/Daily/${roomId}/${this.dateConverter(date)}`
+    );
   }
 
-
-  getMonthlyGraphData(roomId: string, date: Date): Observable<DateValue[]>
-  {
-    return this.http.get<DateValue[]>(`${this.url}/Monthly/${roomId}/${this.dateConverter(date)}`);
+  getMonthlyGraphData(roomId: string, date: Date): Observable<DateValue[]> {
+    return this.http.get<DateValue[]>(
+      `${this.url}/Monthly/${roomId}/${this.dateConverter(date)}`
+    );
   }
 
-
-  getYearlyGraphData(roomId: string): Observable<DateValue[]>
-  {
+  getYearlyGraphData(roomId: string): Observable<DateValue[]> {
     return this.http.get<DateValue[]>(`${this.url}/Yearly/${roomId}`);
   }
 
-  dateConverter(date: Date): string
-  {
+  dateConverter(date: Date): string {
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   }
-
-
-
 }
--- EnergyApp/app/src/app/services/room.service.ts
+++ EnergyApp/app/src/app/services/room.service.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@angular/core';
-import { catchError, interval, map, Observable, of, switchMap, timer } from 'rxjs';
-import { SimpleInfo } from '../models/SimpleInfo';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  Observable,
+  switchMap,
+  timer
+} from 'rxjs';
 import { RoomData } from '../models/RoomData';
+import { SimpleInfo } from '../models/SimpleInfo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoomService {
-
-  url : string = "http://192.168.5.133:2050/api/Room/"
+  // url: string = 'https://localhost:7227/api/Room/';
+  // url : string = "http://192.168.21.7:2050/api/Room/"
+  url: string = 'http://10.233.134.113/api/Room/';
 
   constructor(private http: HttpClient) { }
 
-  getRoomDataStream(roomId : string, intervalMs : number) : Observable<RoomData>
-  {
-    return timer(0,intervalMs).pipe(
-      switchMap(() => this.http.get<RoomData>(this.url +"MeterData/" + roomId))
+  getRoomDataStream(roomId: string, intervalMs: number): Observable<RoomData> {
+    return timer(0, intervalMs).pipe(
+      switchMap(() => this.http.get<RoomData>(this.url + 'MeterData/' + roomId))
     );
   }
 
-  getAllRoomsSimpleInfo() : Observable<SimpleInfo[]> 
-  {
-    return this.http.get<SimpleInfo[]>(this.url + "SimpleInfo");
+  getAllRoomsSimpleInfo(): Observable<SimpleInfo[]> {
+    return this.http.get<SimpleInfo[]>(this.url + 'SimpleInfo');
   }
 
-  getRoomDataOnce(roomId : string) : Observable<RoomData>
-  {
-    return this.http.get<RoomData>(this.url + "MeterData/" + roomId);
+  getRoomDataOnce(roomId: string): Observable<RoomData> {
+    return this.http.get<RoomData>(this.url + 'MeterData/' + roomId);
   }
-
-
 }
